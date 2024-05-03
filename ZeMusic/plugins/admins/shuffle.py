3eb97afc27360d5f903b9779f094e31c038984f2
Familysga <134228968+Familysga@users.noreply.github.com>
2024-05-03 21:50:27 +0300

    Update shuffle.py

--- ZeMusic/plugins/admins/shuffle.py
+++ ZeMusic/plugins/admins/shuffle.py
@@ -11,7 +11,8 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["shuffle", "cshuffle", "خلط"],"") & filters.group & ~BANNED_USERS
+    filters.command(["shuffle", "cshuffle", "خلط"],"")
+    & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def admins(Client, message: Message, _, chat_id):
