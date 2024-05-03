c8adfc2ad4cca366279b6643f961396c84eda7e3
Familysga <134228968+Familysga@users.noreply.github.com>
2024-05-03 21:53:19 +0300

    Update stats.py

--- ZeMusic/plugins/tools/stats.py
+++ ZeMusic/plugins/tools/stats.py
@@ -19,7 +19,8 @@ from ZeMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["الاحصائيات", "ترند","الإحصائيات"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["الاحصائيات", "ترند","الإحصائيات"])
+                & ~BANNED_USERS)
 @language
 async def stats_global(client, message: Message, _):
     upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
