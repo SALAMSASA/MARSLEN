416432d505cfae4d7623ea928f7cbd054dad8091
Familysga <134228968+Familysga@users.noreply.github.com>
2024-05-07 01:57:47 +0300

    Update شعر.py

--- ZeMusic/plugins/play/شعر.py
+++ ZeMusic/plugins/play/شعر.py
@@ -20,3 +20,19 @@ async def ihd(client: Client, message: Message):
             ]
         )
                            )
+
+
+@app.on_message(command(["‹ قصيده ›","قصيده"]) & filters.private)
+async def ihd(client: Client, message: Message):
+    rl = random.randint(3,96)
+    url = f"https://t.me/QasedFaeder/{rl}"
+    await client.send_photo(message.chat.id,url,caption="↯ : تم اختيار قصيده اليك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text=config.CHANNEL_NAME, url=lnk)
+                ],
+            ]
+        )
+    )
