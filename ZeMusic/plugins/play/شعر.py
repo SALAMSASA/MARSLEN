ed9f24af1510dca92b406af295d1bfe615da273e
Familysga <134228968+Familysga@users.noreply.github.com>
2024-05-07 02:08:10 +0300

    Update شعر.py

--- ZeMusic/plugins/play/شعر.py
+++ ZeMusic/plugins/play/شعر.py
@@ -22,16 +22,3 @@ async def ihd(client: Client, message: Message):
                            )
 
 
-@app.on_message(command([f"قصيده", "قصيده🎸", "ق", "{BOT_USERNAME} قصيده"]))
-async def ihd(client: Client, message: Message):
-    rl = random.randint(8,20)
-    url = f"https://t.me/saresnx/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🧚🏼‍♂️ ¦ تم أختياࢪ قصيده لك",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
