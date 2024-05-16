0c1f4cba999f1cf14db38de83a074aaba983b15a
Familysga <134228968+Familysga@users.noreply.github.com>
2024-05-16 09:35:10 +0300

    Update stop.py

--- ZeMusic/plugins/admins/stop.py
+++ ZeMusic/plugins/admins/stop.py
@@ -7,60 +7,35 @@ from ZeMusic.utils.database import set_loop
 from ZeMusic.utils.decorators import AdminRightsCheck
 from ZeMusic.utils.inline import close_markup
 from config import BANNED_USERS
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+from ZeMusic.utils.decorators.admins import admin_check
 
-#import config
+app = Client("my_bot")
 
-#Nem = config.BOT_NAME + " اسكت"
-#Men = config.BOT_NAME + " ايقاف"
-@app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.group & ~BANNED_USERS
-)
-@app.on_message(
-    command(["انهاء","ايقاف"]) & filters.group & ~BANNED_USERS
-)
-@AdminRightsCheck
-async def stop_music(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return
-    await Anony.stop_stream(chat_id)
-    await set_loop(chat_id, 0)
-    await message.reply_text(
-        _["admin_5"].format((message.from_user.mention if message.from_user else message.chat.title)), reply_markup=close_markup(_)
-    )
+# Assuming BANNED_USERS is a set or list of user IDs who are banned
+BANNED_USERS = set()
 
+def command(commands):
+    return filters.command(commands, prefixes=["/", "!", "."])
 
-
-@app.on_message(
-    filters.command(["end", "stop", "cend", "cstop"]) & filters.channel & ~BANNED_USERS
-)
-@app.on_message(
-    command(["انهاء","ايقاف"]) & filters.channel & ~BANNED_USERS
-)
-async def stop_music(cli, message: Message):
-    try:
-        await message.delete()
-    except:
-        pass
-
-    try:
-        language = await get_lang(message.chat.id)
-        _ = get_string(language)
-    except:
-        _ = get_string("en")
-    if message.command[0][0] == "c":
-        chat_id = await get_cmode(message.chat.id)
-        if chat_id is None:
-            return await message.reply_text(_["setting_7"])
-        try:
-            await app.get_chat(chat_id)
-        except:
-            return await message.reply_text(_["cplay_4"])
+async def stop_music(client, message):
+    # Assuming you have a player instance or method to stop the music
+    player = get_music_player_instance()  # استبدل هذا بالكود الفعلي للحصول على مشغل الموسيقى
+    if player is not None:
+        player.stop()  # استبدل هذا بالطريقة الفعلية لإيقاف الموسيقى
+        await message.reply_text("تم إيقاف الموسيقى.")
     else:
-        chat_id = message.chat.id
-    if not len(message.command) == 1:
-        return
-    await Anony.stop_stream(chat_id)
-    await set_loop(chat_id, 0)
-    await message.reply_text(
-        _["admin_5"].format((message.from_user.mention if message.from_user else message.chat.title)), reply_markup=close_markup(_)
-    )
+        await message.reply_text("لم يتم العثور على مشغل الموسيقى.")
+
+def get_music_player_instance():
+    # Replace with actual code to get the music player instance
+    # Example: return music_player_instance
+    return None  # استبدل هذا بالكود الفعلي للحصول على مشغل الموسيقى
+
+@app.on_message(command(["انهاء", "ايقاف"]) & filters.group & ~filters.user(BANNED_USERS) & admin_check)
+async def handle_stop_command(client, message):
+    await stop_music(client, message)
+
+if name == "main":
+    app.run()
