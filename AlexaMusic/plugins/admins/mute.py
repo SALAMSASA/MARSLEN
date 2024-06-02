091d7c17c0ac1eaf855fc03e697309bb90db8f0b
Familysga <134228968+Familysga@users.noreply.github.com>
2024-06-02 13:26:59 +0300

    Update mute.py

--- AlexaMusic/plugins/admins/mute.py
+++ AlexaMusic/plugins/admins/mute.py
@@ -24,7 +24,9 @@ from AlexaMusic.utils.decorators import AdminRightsCheck
 MUTE_COMMAND = get_command("MUTE_COMMAND")
 
 
-@app.on_message(filters.command(MUTE_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+filters.command(MUTE_COMMAND,"")
+    & ~BANNED_USERS)
 @AdminRightsCheck
 async def mute_admin(cli, message: Message, _, chat_id):
     if not len(message.command) == 1 or message.reply_to_message:
