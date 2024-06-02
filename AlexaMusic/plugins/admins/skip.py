3560d112bb736b420be775c5aad9491a6f39bbde
Familysga <134228968+Familysga@users.noreply.github.com>
2024-06-02 13:02:33 +0300

    Update skip.py

--- AlexaMusic/plugins/admins/skip.py
+++ AlexaMusic/plugins/admins/skip.py
@@ -30,7 +30,9 @@ from AlexaMusic.utils.theme import check_theme
 SKIP_COMMAND = get_command("SKIP_COMMAND")
 
 
-@app.on_message(filters.command(SKIP_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(
+filters.command(SKIP_COMMAND,"")
+    & ~BANNED_USERS)
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
     if not len(message.command) < 2:
