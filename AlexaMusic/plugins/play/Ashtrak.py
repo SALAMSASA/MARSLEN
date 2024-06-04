a800b96ffcb3c62168ea7328f126bb128e6fbaf3
Familysga <134228968+Familysga@users.noreply.github.com>
2024-06-05 01:27:14 +0300

    Update Ashtrak.py

--- AlexaMusic/plugins/play/Ashtrak.py
+++ AlexaMusic/plugins/play/Ashtrak.py
@@ -1,13 +1,14 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
-from pyrogram.enums import ChatType
-from pyrogram.errors import UserNotParticipant
-from AlexaMusic import app
+from pyrogram.types import Message, ChatType, InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
 import config
+
+CHANNEL_LINK في config
+if not hasattr(config, 'CHANNEL_LINK'):
+    raise AttributeError("config module does not have 'CHANNEL_LINK' attribute")
 
 channel = config.CHANNEL_LINK
 Nem = config.BOT_NAME + " شغل"
+
 async def subscription(_, __: Client, message: Message):
     if message.from_user is not None:
         user_id = message.from_user.id
@@ -17,11 +18,11 @@ async def subscription(_, __: Client, message: Message):
             print(e)
             return False
     return True
-    
+
 subscribed = filters.create(subscription)
 
 # تعريف دالة لمعالجة الأوامر
-@app.on_message(filters.command(["تشغيل", "شغل",Nem],"") & ~subscribed)
+@app.on_message(filters.command(["تشغيل", "شغل", Nem], "") & ~subscribed)
 async def command_handler(_: Client, message: Message):
     if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
         user_id = message.from_user.id
@@ -33,4 +34,3 @@ async def command_handler(_: Client, message: Message):
             f"◇ عذرًا عزيزي {user} ، عليك الاشتراك في قناة البوت أولاً.",
             reply_markup=markup
         )
-        
