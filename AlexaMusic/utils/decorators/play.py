0c7d2e28477ca8c1da4f2f5b1ed64174af1a890f
Familysga <134228968+Familysga@users.noreply.github.com>
2024-06-04 23:09:59 +0300

    Update play.py

--- AlexaMusic/utils/decorators/play.py
+++ AlexaMusic/utils/decorators/play.py
@@ -156,7 +156,7 @@ def PlayWrapper(command):
                     invitelink = invitelink.replace(
                         "https://t.me/+", "https://t.me/joinchat/"
                     )
-                myu = await message.reply_text(_["call_15"].format(app.mention))
+                message.reply_text(_["call_15"].format(app.mention))
                 try:
                     await asyncio.sleep(1)
                     await userbot.join_chat(invitelink)
