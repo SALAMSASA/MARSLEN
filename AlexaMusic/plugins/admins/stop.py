7496367e26ce5e3871a5e875e18542b76f9fb0df
Familysga <134228968+Familysga@users.noreply.github.com>
2024-06-19 00:49:10 +0300

    Update stop.py

--- AlexaMusic/plugins/admins/stop.py
+++ AlexaMusic/plugins/admins/stop.py
@@ -39,7 +39,9 @@ async def stop_music(cli, message: Message, _, chat_id):
 @app.on_message(
     filters.command(["end", "stop", "cend", "cstop"]) & filters.channel & ~BANNED_USERS
 )
-app.add_handler(MessageHandler(stop_command_handler, filters.command(["اسكت", "ايقاف"]) & filters.channel & ~filters.user(BANNED_USERS)
+app.add_handler(MessageHandler(stop_command_handler, filters.command(["اسكت", "ايقاف"]) & filters.channel & ~filters.user(BANNED_USERS)))
+
+app.run()
 
 async def stop_music(cli, message: Message):
     try:
