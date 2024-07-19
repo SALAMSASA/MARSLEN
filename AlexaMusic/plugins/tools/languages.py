e5829284543ad245df1b7ee392e22771495a0abf
Familysga <134228968+Familysga@users.noreply.github.com>
2024-07-19 12:37:49 +0300

    Update languages.py

--- AlexaMusic/plugins/tools/languages.py
+++ AlexaMusic/plugins/tools/languages.py
@@ -27,7 +27,7 @@ def lanuages_keyboard(_):
     keyboard = InlineKeyboard(row_width=2)
     keyboard.row(
         InlineKeyboardButton(
-            text="🇦🇺 ᴇɴɢʟɪsʜ 🇦🇺",
+            text="🇮🇶 العربيه 🇮🇶",
             callback_data=f"languages:en",
         ),
         InlineKeyboardButton(
