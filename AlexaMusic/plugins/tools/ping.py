6651be424d45e99f91008469d60d840c590a7529
Familysga <134228968+Familysga@users.noreply.github.com>
2024-07-19 12:32:22 +0300

    Update ping.py

--- AlexaMusic/plugins/tools/ping.py
+++ AlexaMusic/plugins/tools/ping.py
@@ -26,7 +26,8 @@ from AlexaMusic.utils.decorators.language import language
 PING_COMMAND = get_command("PING_COMMAND")
 
 
-@app.on_message(filters.command(PING_COMMAND) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(PING_COMMAND, "")
+    & filters.group    & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
     response = await message.reply_photo(
