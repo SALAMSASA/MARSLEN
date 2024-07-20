d9689de030343a788ebc88b0a5fe41d2428c0953
Familysga <134228968+Familysga@users.noreply.github.com>
2024-07-20 23:05:21 +0300

    Update songs.py

--- AlexaMusic/plugins/tools/songs.py
+++ AlexaMusic/plugins/tools/songs.py
@@ -35,7 +35,8 @@ SONG_COMMAND = get_command("SONG_COMMAND")
 
 
 @app.on_message(filters.command(SONG_COMMAND, "")
-    & filters.group    & ~BANNED_USERS)
+    & filters.group    & ~BANNED_USERS
+)
 @language
 async def song_commad_group(client, message: Message, _):
     upl = InlineKeyboardMarkup(
@@ -55,7 +56,8 @@ async def song_commad_group(client, message: Message, _):
 
 
 @app.on_message(filters.command(SONG_COMMAND, "")
-    & filters.group    & ~BANNED_USERS)
+    & filters.group    & ~BANNED_USERS
+)
 @language
 async def song_commad_private(client, message: Message, _):
     await message.delete()
