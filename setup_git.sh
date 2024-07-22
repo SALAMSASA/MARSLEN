3ef924b09833ed9c9365be8a7fcfaf0720a31a05
Familysga <134228968+Familysga@users.noreply.github.com>
2024-07-22 22:18:52 +0300

    Update setup_git.sh

--- setup_git.sh
+++ setup_git.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-# Get the GitHub token from the environment variable
-GITHUB_TOKEN=${GITHUB_TOKEN:?GITHUB_TOKEN environment variable not set}
-
-# Set the repository URL with token
-REPO_URL="https://${GITHUB_TOKEN}@github.com/username/repository.git"
-
-# Update the remote URL
-git remote set-url origin $REPO_URL
-
-# Fetch from origin
-git fetch -v -- origin
+git add .
+git commit -m "Add setup script for GitHub token"
+git push heroku main
