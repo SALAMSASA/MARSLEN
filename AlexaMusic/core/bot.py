92f85acb354158173c419c9bc2ef7335caa27513
Familysga <134228968+Familysga@users.noreply.github.com>
2024-11-10 23:22:22 +0300

    Update bot.py

--- AlexaMusic/core/bot.py
+++ AlexaMusic/core/bot.py
@@ -25,7 +25,9 @@ class AlexaBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
+            sleep_threshold=180,
+            max_concurrent_transmissions=4,
+            workers=50,
         )
         LOGGER(__name__).info(f"Starting Bot...")
 
